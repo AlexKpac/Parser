8b84d45b4606e1d5b6e214749b50b4ce3deca2ef
Никита <nickit94>
2020-10-05 22:56:12 +0700

    Lesson completed

--- parser.py
+++ parser.py
@@ -1,11 +1,26 @@
 import logging
-
 import bs4
 import requests
-
+import collections
+import csv
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('wb')
+
+ParseResult = collections.namedtuple(
+    'ParseResult',
+    (
+        'brand_name',
+        'goods_name',
+        'url',
+    ),
+)
+
+HEADERS = (
+    'Бренд',
+    'Товар',
+    'Ссылка',
+)
 
 class Client:
 
@@ -16,6 +31,7 @@ class Client:
                           'Chrome/85.0.4183.83 YaBrowser/20.9.0.933 Yowser/2.5 Safari/537.36',
             'Accept-Language': 'ru',
         }
+        self.result = []
 
     def load_page(self):
         url = 'https://www.wildberries.ru/catalog/elektronika/smart-chasy'
@@ -30,5 +46,62 @@ class Client:
             self.parse_block(block=block)
 
     def parse_block(self, block):
-        logger.info(block)
-        logger.info('=' * 100)
+        #logger.info(block)
+        #logger.info('=' * 100)
+
+        url_block = block.select_one('a.ref_goods_n_p')
+        if not url_block:
+            logger.error('no url_block')
+            return
+
+        url = url_block.get('href')
+        if not url:
+            logger.error('no href')
+            return
+
+        name_block = block.select_one('div.dtlist-inner-brand-name')
+        if not name_block:
+            logger.error(f'no name_block on {url}')
+            return
+
+        brand_name = name_block.select_one('strong.brand-name')
+        if not brand_name:
+            logger.error(f'no brand_name on {url}')
+            return
+
+        brand_name = brand_name.text
+        brand_name = brand_name.replace('/', '').strip()
+
+        goods_name = name_block.select_one('span.goods-name')
+        if not goods_name:
+            logger.error(f'no goods_name on {url}')
+            return
+
+        goods_name = goods_name.text.strip()
+
+        self.result.append(ParseResult(
+            url=url,
+            brand_name=brand_name,
+            goods_name=goods_name,
+        ))
+
+        logger.debug('%s, | %s | %s', url, brand_name, goods_name)
+
+    def save_result(self):
+        path = '/Users/Никита/Desktop/goods.csv'
+        with open(path, 'w') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(HEADERS)
+            for item in self.result:
+                writer.writerow(item)
+
+    def run(self):
+        text = self.load_page()
+        self.parse_page(text)
+        logger.info(f'Получили {len(self.result)} элементов')
+
+        self.save_result()
+
+if __name__ == '__main__':
+    parser = Client()
+    parser.run()
