cabeacbcb56f98c57e55c0698ccdd8ddab59bcfd
Никита <nickit94>
2020-10-10 20:57:44 +0700

    Построил примерную структуру класса

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import logging
 import collections
 import time
 
+from enum import Enum
 import selenium.common.exceptions as se
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -41,6 +42,12 @@ HEADERS = (
 )
 CURRENT_CITY = 'Новосибирск'
 WD_PATH = "venv/WebDriverManager/chromedriver.exe"
+WAIT_BETWEEN_PAGES_SEC = 3
+
+
+class ParseWhat(Enum):
+    PRODUCT_PAGE = 1
+    CATALOG_PAGE = 2
 
 
 class Parser:
@@ -50,6 +57,7 @@ class Parser:
         # options.add_argument("window-size=1,1")
         self.driver = webdriver.Chrome(executable_path=WD_PATH, options=options)
         self.wait = WebDriverWait(self.driver, 10)
+        self.cur_page = 1
 
     # Поиск элемента с таймаутом
     def __find_elem_with_timeout(self, by, elem):
@@ -88,7 +96,17 @@ class Parser:
                 # Отправка нужного города
                 input_city.send_keys(CURRENT_CITY, Keys.ENTER)
 
-    def load_one_page(self, url):
+    # Метод для парсинга html страницы продукта
+    def __parse_product_page(self, html):
+        print(html)
+        return
+
+    # Метод для парсинга html страницы каталога
+    def __parse_catalog_page(self, html):
+        return
+
+    # Запуск браузера, загрузка начальной страницы парсинга, выбор города
+    def wd_open_browser(self, url):
         self.driver.get(url)
 
         # Ожидание загрузки цен, таймаут = 10, в случае исключения - выход
@@ -99,24 +117,57 @@ class Parser:
         # Выбор города
         self.__city_selection()
 
-        logger.info("Цены загружены успешно, передача html кода")
-        content = self.driver.page_source
-        # self.driver.close()
-        return content
+    # Получить текущий код страницы
+    def wd_get_cur_page(self):
+        return self.driver.page_source
 
-    def load_page(self, url):
-        self.driver.get(url)
+    # Переход на заданную страницу num_page через клик (для имитации пользователя)
+    def wd_next_page(self):
+        self.cur_page += 1
+        try:
+            num_page_elem = self.driver.find_element_by_xpath(
+                f"//li[@class='pagination-widget__page ']/a[text()='{self.cur_page}']")
+            num_page_elem.click()
 
-        # Ожидание загрузки цен, таймаут = 10, в случае исключения - выход
-        if not self.__find_elem_with_timeout(By.CLASS_NAME, "product-min-price__current"):
-            self.driver.quit()
-            return None
+            # Ждем, пока на новой странице не подгрузятся цены, только потом передаем управление
+            if not self.__find_elem_with_timeout(By.CLASS_NAME, "product-min-price__current"):
+                logger.info(f"Не удалось подгрузить цены на '{self.cur_page}' странице")
+                return False
 
-        num_page = 4
-        # Переход на заданную страницу num_page через клик (для имитации пользователя)
-        self.driver.find_element_by_xpath(f"//li[@class='pagination-widget__page ']/a[text()='{num_page}']").click()
+            time.sleep(WAIT_BETWEEN_PAGES_SEC)
+            return True
+
+        # Если страница не найдена - достигнут конец каталога
+        except se.NoSuchElementException:
+            logger.info("Достигнут конец каталога")
+            return False
+
+    # Завершение работы браузера
+    def wd_close_browser(self):
+        logger.info("Завершение работы")
+        self.driver.quit()
+
+    # Запуск работы парсера для каталога
+    def run_catalog(self, url):
+        self.wd_open_browser(url)
+
+        while True:
+            html = self.wd_get_cur_page()
+            self.__parse_catalog_page(html)
+
+            if not self.wd_next_page():
+                break
+
+        self.wd_close_browser()
+
+    # Запуск работы парсера для продукта
+    def run_product(self, url):
+        self.wd_open_browser(url)
+        html = self.wd_get_cur_page()
+        self.__parse_product_page(html)
+        self.wd_close_browser()
 
 
 if __name__ == '__main__':
     parser = Parser()
-    parser.load_one_page("https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/")
+    parser.run_product("https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/")
