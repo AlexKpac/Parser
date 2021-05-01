7435c27ab5a348efb36845c7250d9a1058825a5a
Nick <nickit.94-94@mail.ru>
2021-05-01 23:45:45 +0700

    Переделана структура всего проекта. Все парсеры переведены на базовый класс.

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import csv
 import configparser
 from time import time, sleep
 import os
+import sys
 import socket
 
-from data_source.parsers.dns_parse import DNSParse
-from data_source.parsers.mvideo_parse import MVideoParse
-from data_source.parsers.mts_parse import MTSParse
-from data_source.parsers.eldorado_parse import EldoradoParse
-from data_source.parsers.citilink_parse import CitilinkParse
-from core.checker import Checker
-from output.telegram.bot import Bot
-import common.general_helper as h
+
+from modules.data_receiver.parsers.dns_parse import DNSParse
+from modules.data_receiver.parsers.mvideo_parse import MVideoParse
+from modules.data_receiver.parsers.mts_parse import MTSParse
+from modules.data_receiver.parsers.eldorado_parse import EldoradoParse
+from modules.data_receiver.parsers.citilink_parse import CitilinkParse
+from modules.db_inserter.db_inserter import DbInserter
+from modules.data_checker.data_checker import DataChecker
+from modules.data_sender.telegram.bot import Bot
+import modules.common.helper as h
 
 logger = h.logging.getLogger('main')
+MAX_COUNT_CRASH_FOR_ALARM = 3
 
 
 # Проверка наличия подключения к сети
@@ -48,7 +52,8 @@ def load_exceptions_model_names():
 
 # Чтение списка разрешенных названий моделей для добавления в БД
 def load_allowed_model_names_list_for_base():
-    with open(h.PATH_LIST_MODEL_NAMES_BASE, 'r', encoding='UTF-8') as f:
+    print(h.ROOT_DIR, h.LIST_MODEL_NAMES_BASE_PATH)
+    with open(h.LIST_MODEL_NAMES_BASE_PATH, 'r', encoding='UTF-8') as f:
         h.ALLOWED_MODEL_NAMES_LIST_FOR_BASE = f.read().splitlines()
 
 
@@ -88,23 +93,44 @@ def save_result_list(elements):
 
 # Чтение данных
 def read_config():
+    h.ROOT_DIR = sys.path[1] + "\\"
+
     config = configparser.ConfigParser()
-    config.read('config.ini', encoding="utf-8")
+    config.read(h.ROOT_DIR + 'config.ini', encoding="utf-8")
     h.REBUILT_IPHONE_NAME = ' ' + config.defaults()['rebuilt_iphone_name']
     h.IGNORE_WORDS_FOR_COLOR = config['parser']['color_ignore'].lower().split('\n')
 
 
 # Удалить лок-файл
 def del_lock_file():
-    if os.path.isfile(h.PATH_UNDEFINED_MODEL_NAME_LIST_LOCK):
-        os.remove(h.PATH_UNDEFINED_MODEL_NAME_LIST_LOCK)
+    if os.path.isfile(h.UNDEFINED_MODEL_NAME_LIST_LOCK_PATH):
+        os.remove(h.UNDEFINED_MODEL_NAME_LIST_LOCK_PATH)
 
 
 # Создать лок файл, запрещающий сервисному боту читать файл с исключениями
 def create_lock_file():
     del_lock_file()
-    with open(h.PATH_UNDEFINED_MODEL_NAME_LIST_LOCK, 'w') as f:
+    with open(h.UNDEFINED_MODEL_NAME_LIST_LOCK_PATH, 'w') as f:
         pass
+
+
+def read_count_crash_for_alarm(count_crash):
+    """
+    Чтение файла с информацией о поломках
+    """
+    with open(h.CRASH_DATA_PATH, 'r', encoding='UTF-8') as f:
+        line1 = f.readline().replace('\n', '')
+        count_crash = int(line1) if line1 else 0
+
+        logger.info("Count Crash = {}".format(count_crash))
+
+
+def write_count_crash_for_alarm(count_crash):
+    """
+    Запись файла с информацией о поломках
+    """
+    with open(h.CRASH_DATA_PATH, 'w', encoding='UTF-8') as f:
+        f.write(str(count_crash))
 
 
 def worker1():
@@ -127,7 +153,28 @@ def worker4():
     parser4.run_catalog("https://www.eldorado.ru/c/smartfony/")
 
 
+def send_alarm_in_telegram(msg):
+    pass
+
+
+def inc_count_crash(count_crash, shop_name):
+    logger.error("Упал {}, Count Crash = {}".format(shop_name, count_crash))
+    count_crash += 1
+
+    if count_crash == MAX_COUNT_CRASH_FOR_ALARM:
+        send_alarm_in_telegram("Почини меня")
+        return
+
+    write_count_crash_for_alarm(count_crash)
+
+
+def clear_count_crash(count_crash):
+    count_crash = 0
+    write_count_crash_for_alarm(count_crash)
+
+
 if __name__ == '__main__':
+    COUNT_CRASH = 0
 
     # Проверка наличия интернета перед выполнением программы
     # if not check_internet():
@@ -165,52 +212,53 @@ if __name__ == '__main__':
     h.del_old_logs()
     result_list = []
 
+    read_config()
     load_allowed_model_names_list_for_base()
     load_exceptions_model_names()
-    read_config()
     create_lock_file()
-    #
+
     parser = MVideoParse()
     result = parser.run_catalog("https://www.mvideo.ru/smartfony-i-svyaz-10/smartfony-205?sort=price_asc")
     # result = load_result_from_csv("mvideo.csv")
     if not result:
-        raise SystemExit(5)
+        inc_count_crash(COUNT_CRASH, "Мвидео")
     result_list.extend(result)
-    #
-    # parser = MTSParse()
-    # result = parser.run_catalog("https://shop.mts.ru/catalog/smartfony/")
-    # # result = load_result_from_csv("mts.csv")
-    # if not result:
-    #     raise SystemExit(5)
-    # result_list.extend(result)
-    #
-    # parser = DNSParse()
-    # result = parser.run_catalog("https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/")
-    # # result = load_result_from_csv("dns.csv")
-    # if not result:
-    #     raise SystemExit(5)
-    # result_list.extend(result)
-    #
-    # parser = CitilinkParse()
-    # result = parser.run_catalog("https://www.citilink.ru/catalog/mobile/smartfony/")
-    # # result = load_result_from_csv("citilink.csv")
-    # if not result:
-    #     raise SystemExit(5)
-    # result_list.extend(result)
-    #
-    # parser = EldoradoParse()
-    # result = parser.run_catalog("https://www.eldorado.ru/c/smartfony/")
-    # # result = load_result_from_csv("eldorado.csv")
-    # if not result:
-    #     raise SystemExit(5)
-    # result_list.extend(result)
-    #
+
+    parser = MTSParse()
+    result = parser.run_catalog("https://shop.mts.ru/catalog/smartfony/")
+    # result = load_result_from_csv("mts.csv")
+    if not result:
+        inc_count_crash(COUNT_CRASH, "МТС")
+    result_list.extend(result)
+
+    parser = DNSParse()
+    result = parser.run_catalog("https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/")
+    # result = load_result_from_csv("dns.csv")
+    if not result:
+        inc_count_crash(COUNT_CRASH, "ДНС")
+    result_list.extend(result)
+
+    parser = CitilinkParse()
+    result = parser.run_catalog("https://www.citilink.ru/catalog/mobile/smartfony/")
+    # result = load_result_from_csv("citilink.csv")
+    if not result:
+        inc_count_crash(COUNT_CRASH, "Ситилинк")
+    result_list.extend(result)
+
+    parser = EldoradoParse()
+    result = parser.run_catalog("https://www.eldorado.ru/c/smartfony/")
+    # result = load_result_from_csv("eldorado.csv")
+    if not result:
+        inc_count_crash(COUNT_CRASH, "Эльдорадо")
+    result_list.extend(result)
+
     del_lock_file()
+    clear_count_crash(COUNT_CRASH)
     save_result_list(result_list)
 
     # result_list = load_result_from_csv("goods2.csv")
-    # check = Checker(result_list)
-    # benefit_price_list = check.run()
+    check = Checker(result_list)
+    benefit_price_list = check.run()
     #
     # bot = Bot()
     # bot.checking_irrelevant_posts(result_list)
